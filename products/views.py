54c562be6c3dc18c68b83c082441854d309c4b87
sravan-kumar-ta <sravan0095@gmail.com>
2022-12-21 13:56:59 +0530

    filtering products changed to without ajax

--- products/views.py
+++ products/views.py
@@ -40,39 +40,46 @@ def product_list(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(Q(category=category) & Q(stock__gte=1))
     brands = Brand.objects.filter(category=category)
-    context = {
+
+    context = dict()
+
+    # filtering the products
+    if request.method == 'POST':
+        selected_brands = request.POST.getlist('brands')
+        sort_method = request.POST['sort']
+        min_price = request.POST['min']
+        max_price = request.POST['max']
+
+        context.update({
+            'selected_brands': selected_brands,
+            'sort_method': sort_method,
+            'min_price': min_price,
+            'max_price': max_price
+        })
+
+        if len(selected_brands) != 0:
+            selected_brands_objs = list()
+            for brand in selected_brands:
+                selected_brands_objs.append(get_object_or_404(Brand, name=brand))
+            products = products.filter(brand__in=selected_brands_objs)
+
+        if sort_method:
+            if sort_method == '1':
+                products = products.order_by('price')
+            elif sort_method == '2':
+                products = products.order_by('-price')
+
+        if min_price:
+            products = products.filter(price__gte=min_price)
+        if max_price:
+            products = products.filter(price__lte=max_price)
+
+    context.update({
         'category': category,
         'products': products,
         'brands': brands
-    }
+    })
     return render(request, 'products/category_products.html', context)
-
-
-def products_filter(request):
-    time.sleep(2)
-    category = request.GET.getlist('category')
-    minPrice = request.GET['minPrice']
-    maxPrice = request.GET['maxPrice']
-    selected_brands = request.GET.getlist('brands[]')
-    price_range = request.GET['price_range']
-
-    category_obj = get_object_or_404(Category, title=category[0])
-    all_products = Product.objects.filter(Q(category=category_obj) & Q(stock__gte=1))
-    all_products = all_products.filter(Q(price__gte=minPrice) & Q(price__lte=maxPrice))
-    if len(selected_brands) is not 0:
-        selected_brands_objs = list()
-        for brand in selected_brands:
-            selected_brands_objs.append(get_object_or_404(Brand, name=brand))
-        all_products = all_products.filter(brand__in=selected_brands_objs)
-
-    if price_range:
-        if price_range is '1':
-            all_products = all_products.order_by('price')
-        elif price_range is '2':
-            all_products = all_products.order_by('-price')
-
-    template = render_to_string('ajax/products.html', {'products': all_products})
-    return JsonResponse({'data': template})
 
 
 def details_of_medicine(request, c_slug, p_slug):
